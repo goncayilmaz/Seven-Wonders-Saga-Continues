6c49160c3a3f619c4aab3ed9546f825481280846
efe eroglu <efe.eroglu1996@gmail.com>
2019-11-18 18:55:51 +0300

    menu view gui si yapıldı fx controller planlandı. main ile bağlantı planlandı

--- src/superProject/Controller/Menu.java
+++ src/superProject/Controller/Menu.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sun.font.FontConfigManager;
 
 public class Menu extends Application {
 
@@ -14,22 +13,21 @@ public class Menu extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-    try{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/View/MenuViewFX.fxml"));
-        Scene scene = new Scene(root,400,400);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MenuViewFX.fxml"));
+            primaryStage.setTitle("Hello World");
 
+            primaryStage.setScene(new Scene(root, 750, 500));
 
-
-
-    }
-    catch (Exception e){
-        e.printStackTrace();
-    }
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
+
 
     public static void main(String[] args) {
         launch(args);
--- src/superProject/Main.java
+++ src/superProject/Main.java
@@ -1,15 +1,31 @@
 package superProject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import superProject.Controller.GameEngine;
-
-import java.io.File;
-import java.net.URL;
 
 
 public class Main extends Application {
@@ -17,6 +33,32 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("MenuViewFX.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+       /* Parent root = FXMLLoader.load(getClass().getResource("MenuViewFX.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        */
+        /*
+        Label label = new Label("Hello World");
+        label.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(label, 500, 350);
+
+        primaryStage.setTitle("Hello World Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+         */
+        /*
+
 
         try{
 
@@ -36,9 +78,11 @@ public class Main extends Application {
         catch (Exception e){
             e.printStackTrace();
         }
-
+*/
 
     }
+
+
 
     public static void main(String[] args) {
 
