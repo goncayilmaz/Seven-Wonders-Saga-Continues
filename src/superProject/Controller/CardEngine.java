06e1e5ad2d63d59aab3a037eb3ec5c6c75b16626
efe eroglu <efe.eroglu1996@gmail.com>
2019-11-18 23:50:37 +0300

    card view yapıldı controller bağlandı

--- src/superProject/Controller/CardEngine.java
+++ src/superProject/Controller/CardEngine.java
@@ -1,4 +1,33 @@
 package superProject.Controller;
 
-public class CardEngine {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CardEngine extends Application {
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../View/CardViewFX.fxml"));
+            primaryStage.setTitle("Card View");
+
+            primaryStage.setScene(new Scene(root, 200, 300));
+            // primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 }
--- src/superProject/View/CardView.java
+++
@@ -1,7 +0,0 @@
-package superProject.View;
-
-public class CardView {
-
-
-
-}
