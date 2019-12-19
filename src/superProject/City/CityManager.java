57963719ed359530dfb711d31ddf2d74f0e71a62
AtaberkGozkaya <ataberkgozkaya1071@gmail.com>
2019-12-19 23:05:30 +0300

    createcity yapıldı ama bazı board levelların materyal dışında getirileri var onlar daha ayarlanmadı getPlayerCity metodu eklendi

--- src/superProject/City/CityManager.java
+++ src/superProject/City/CityManager.java
@@ -506,11 +506,11 @@ public class CityManager extends Application {
      * @param cityName
      * @return chosenCity
      */
-    public City getPlayerCity(int boardType, String cityName)
+    public City getPlayerCity(boolean boardType, String cityName)
     {
         City chosenCity = null;
 
-        if(boardType == 0)
+        if(boardType == true)
         {
             for(int i = 0; i < citiesA.size(); i++)
             {
@@ -533,9 +533,9 @@ public class CityManager extends Application {
         return chosenCity;
     }
 
-    public int getNumberOfCities(int boardType)
+    public int getNumberOfCities(boolean boardType)
     {
-        if(boardType == 0)
+        if(boardType == true)
             return citiesA.size();
         else
             return citiesB.size();
