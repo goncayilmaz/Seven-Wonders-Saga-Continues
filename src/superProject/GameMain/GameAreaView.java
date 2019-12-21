b27740eb5e7adc2efaeaed70453f6b1d369ff352
efe eroglu <efe.eroglu1996@gmail.com>
2019-12-21 04:01:29 +0300

    game areaview ile geliştirmeler yapıldı kartların imageleri random ele geldi game engine de animasyon için path bakıldı. cardview değişmeli sadece image olmalı orasına bakılcak

--- src/superProject/GameMain/GameAreaView.java
+++ src/superProject/GameMain/GameAreaView.java
@@ -1,4 +1,96 @@
 package superProject.GameMain;
 
-public class GameAreaView {
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import superProject.GameProperties.CardEngine;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameAreaView implements Initializable {
+
+
+
+    @FXML
+    private ImageView fifthCard;
+
+    @FXML
+    private ImageView seventhCard;
+
+    @FXML
+    private ImageView thirdCard;
+
+    @FXML
+    private ImageView firstCard;
+
+    @FXML
+    private ImageView forthCard;
+
+    @FXML
+    private ImageView sixthCard;
+
+    @FXML
+    private ImageView secondCard;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        CardEngine cardEngine = new CardEngine();
+        cardEngine.createFirstAgeCards(7);
+
+        String preCard= "/Images/images/cards/";
+        String cardName=cardEngine.getFirstAgeCards().get(0).getPhotoName();
+
+        Image image = new Image(getClass().getResourceAsStream(preCard+cardName));
+        firstCard.setImage(image);
+
+
+        String cardName1=cardEngine.getFirstAgeCards().get(1).getPhotoName();
+        Image image1 = new Image(getClass().getResourceAsStream(preCard+cardName1));
+        secondCard.setImage(image1);
+
+
+        String cardName2=cardEngine.getFirstAgeCards().get(2).getPhotoName();
+        Image image2 = new Image(getClass().getResourceAsStream(preCard+cardName2));
+        thirdCard.setImage(image2);
+
+
+        String cardName3=cardEngine.getFirstAgeCards().get(3).getPhotoName();
+        Image image3 = new Image(getClass().getResourceAsStream(preCard+cardName3));
+        forthCard.setImage(image3);
+
+
+        String cardName4=cardEngine.getFirstAgeCards().get(4).getPhotoName();
+        Image image4 = new Image(getClass().getResourceAsStream(preCard+cardName4));
+        fifthCard.setImage(image4);
+   
+
+        FadeTransition fadeInTransition = new FadeTransition(Duration.millis(1500), fifthCard);
+        fadeInTransition.setFromValue(1.0);
+        fadeInTransition.setToValue(0.0);
+
+        //fifthCard.setTranslateX(250);
+        //fifthCard.setTranslateY(123);
+
+
+        String cardName5=cardEngine.getFirstAgeCards().get(5).getPhotoName();
+        Image image5 = new Image(getClass().getResourceAsStream(preCard+cardName5));
+        sixthCard.setImage(image5);
+
+
+        String cardName6=cardEngine.getFirstAgeCards().get(6).getPhotoName();
+        Image image6 = new Image(getClass().getResourceAsStream(preCard+cardName6));
+        seventhCard.setImage(image6);
+
+
+
+
+    }
 }
