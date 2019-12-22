b62f57c9de3a54f68ad48aa3cea79b9c17d5e7d3
utkukalkanli <a10i73650>
2019-12-23 01:44:44 +0300

    player'ın oyun sonu puanını saydırma city'de boardlevellar düzenlendi

--- src/superProject/Controller/CardEngine.java
+++ src/superProject/Controller/CardEngine.java
@@ -17,17 +17,6 @@ public class CardEngine extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("../View/CardViewFX.fxml"));
-            primaryStage.setTitle("Card View");
-
-            primaryStage.setScene(new Scene(root, 200, 300));
-            // primaryStage.setResizable(false);
-            primaryStage.show();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
 
 
 
